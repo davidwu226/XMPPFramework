2c2e253d0d594084bce210fd3d8a96b7d908389a
David <davidwu226@gmail.com>
2015-10-30 00:37:24 -0700

    Support for Carthage.

---
+++ XMPPFramework.h
@@ -0,0 +1,20 @@
+//
+//  XMPPFramework.h
+//  XMPPFramework
+//
+//  Created by David on 10/29/15.
+//  Copyright © 2015 XMPPFramework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMPPFramework.
+FOUNDATION_EXPORT double XMPPFrameworkVersionNumber;
+
+//! Project version string for XMPPFramework.
+FOUNDATION_EXPORT const unsigned char XMPPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPPFramework/PublicHeader.h>
+
+
+#import "XMPP.h"
